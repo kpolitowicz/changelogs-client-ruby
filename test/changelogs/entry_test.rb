6cf9e9ef8f07a1231aae535108d3127ea5cd5523
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-23 17:32:34 +0700

    test: mocked the exact POST call

--- test/changelogs/entry_test.rb
+++ test/changelogs/entry_test.rb
@@ -22,6 +22,11 @@ module Changelogs
     end
 
     def test_that_it_creates_new_entry
+      stub_request(:post, "https://changelogs.nimonikapp.com/entries").
+          with(:body => "{\"data\":{\"type\":\"entries\",\"attributes\":{\"event\":\"C\",\"changeset\":\"{}\",\"account_id\":123,\"user_id\":234,\"user_type\":\"F\",\"logged_type\":\"SomeModel\",\"logged_id\":345,\"logged_at\":\"2001-02-03T04:05:06+00:00\"}}}",
+                      :headers => {'Accept'=>'application/vnd.api+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/vnd.api+json', 'User-Agent'=>'Faraday v0.11.0'}).
+        to_return(:status => 200, :body => "", :headers => {})
+
       entry = Entry.create(@@valid_args)
 
       @@valid_args.each do |key, val|
