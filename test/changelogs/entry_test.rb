7545a0f136a102a716f78f50692ebe3eb9c92699
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-23 17:12:04 +0700

    test: refactored to hold valid attrs in a var

--- test/changelogs/entry_test.rb
+++ test/changelogs/entry_test.rb
@@ -2,17 +2,19 @@ require 'test_helper'
 
 module Changelogs
   class EntryTest < Minitest::Test
+    @@valid_args = {
+      event:       'C',
+      changeset:   '{}',
+      account_id:  123,
+      user_id:     234,
+      user_type:   'F',
+      logged_type: 'SomeModel',
+      logged_id:   345,
+      logged_at:   DateTime.parse('2001-02-03 04:05:06 UTC'),
+    }
+
     def test_that_it_inits_new_entry
-      entry = Entry.new(
-        event:       'C',
-        changeset:   '{}',
-        account_id:  123,
-        user_id:     234,
-        user_type:   'F',
-        logged_type: 'SomeModel',
-        logged_id:   345,
-        logged_at:   DateTime.parse('2001-02-03 04:05:06 UTC'),
-      )
+      entry = Entry.new(@@valid_args)
 
       assert_equal entry.event, 'C'
     end
