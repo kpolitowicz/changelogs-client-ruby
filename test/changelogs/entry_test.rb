5651e3cf0b3c5017031d9ff4a011ae9b2a6f6b7e
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-23 17:34:25 +0700

    cleanup: no need for new test (duplicates the create one)

--- test/changelogs/entry_test.rb
+++ test/changelogs/entry_test.rb
@@ -13,14 +13,6 @@ module Changelogs
       logged_at:   DateTime.parse('2001-02-03 04:05:06 UTC'),
     }
 
-    def test_that_it_inits_new_entry
-      entry = Entry.new(@@valid_args)
-
-      @@valid_args.each do |key, val|
-        assert_equal entry.public_send(key), val, "Wrong entry.#{key}"
-      end
-    end
-
     def test_that_it_creates_new_entry
       stub_request(:post, "https://changelogs.nimonikapp.com/entries").
           with(:body => "{\"data\":{\"type\":\"entries\",\"attributes\":{\"event\":\"C\",\"changeset\":\"{}\",\"account_id\":123,\"user_id\":234,\"user_type\":\"F\",\"logged_type\":\"SomeModel\",\"logged_id\":345,\"logged_at\":\"2001-02-03T04:05:06+00:00\"}}}",
