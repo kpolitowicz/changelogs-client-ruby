19f63d78db38f0b6a2bfa9641c64b10703a3350e
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-24 13:52:22 +0700

    cleanup: removed Changelogs tests that are duplicated in Entry tests (configurable subdomain)

--- test/changelogs_test.rb
+++ test/changelogs_test.rb
@@ -1,21 +1,7 @@
 require 'test_helper'
 
 class ChangelogsTest < Minitest::Test
-  def teardown
-    Changelogs.subdomain = nil
-  end
-
   def test_that_it_has_a_version_number
     refute_nil Changelogs::VERSION
-  end
-
-  def test_that_api_subdomain_is_assignable
-    Changelogs.subdomain = 'subdomain'
-
-    assert_equal Changelogs.api_host, 'https://subdomain.nimonikapp.com/'
-  end
-
-  def test_that_api_subdomain_has_default
-    assert_equal Changelogs.api_host, 'https://changelogs.nimonikapp.com/'
   end
 end
