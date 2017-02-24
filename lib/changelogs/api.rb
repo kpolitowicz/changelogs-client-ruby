95ecbb5f5a02f3e9df5bc993195352942feaff72
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-24 18:28:56 +0700

    feat: change post to send JSON

--- lib/changelogs/api.rb
+++ lib/changelogs/api.rb
@@ -4,7 +4,11 @@ module Changelogs
   class API
     def self.post(attrs)
       conn = Faraday.new(url: Changelogs.api_host)
-      conn.post '/entries', attrs
+      conn.post do |req|
+          req.url '/entries'
+          req.headers['Content-Type'] = 'application/json'
+          req.body = attrs.to_json
+      end
     end
   end
 end
