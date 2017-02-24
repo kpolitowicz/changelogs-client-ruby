bcee040c5ba54c716b634b8d304399956fdef5fb
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-24 13:14:21 +0700

    config: added configurable API host

--- lib/changelogs.rb
+++ lib/changelogs.rb
@@ -1,3 +1,8 @@
 require "changelogs/version"
 require "changelogs/entry"
 
+module Changelogs
+  class << self
+    attr_accessor :api_host
+  end
+end
--- test/changelogs_test.rb
+++ test/changelogs_test.rb
@@ -4,4 +4,10 @@ class ChangelogsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil Changelogs::VERSION
   end
+
+  def test_that_api_host_is_assignable
+    Changelogs.api_host = 'http://example.com/'
+
+    assert_equal Changelogs.api_host, 'http://example.com/'
+  end
 end
