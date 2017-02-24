55de47fe0f27a417c9be01075b0beb8fcc05de2c
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-24 12:56:25 +0700

    refac: no need for ::

--- test/changelogs_test.rb
+++ test/changelogs_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class ChangelogsTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Changelogs::VERSION
+    refute_nil Changelogs::VERSION
   end
 end
