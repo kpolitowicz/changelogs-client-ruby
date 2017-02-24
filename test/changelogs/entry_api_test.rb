ac11d7f22aae78ccdfbcced2bf10fa99ed0ebf2b
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-24 18:47:41 +0700

    test: removed unneeded setup before test

--- test/changelogs/entry_api_test.rb
+++ test/changelogs/entry_api_test.rb
@@ -25,9 +25,6 @@ module Changelogs
     end
 
     def test_that_it_uses_default_api_subdomain
-      stub_request(:post, "https://changelogs.nimonikapp.com/entries").
-        to_return(status: 200, body: "", headers: {})
-
       entry = Entry.create()
 
       assert_requested :post, "https://changelogs.nimonikapp.com/entries"
