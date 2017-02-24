6a5702fe2f678f5cad0c43bd532e56bdff4496c8
Kamil Politowicz <k.politowicz@gmail.com>
2017-02-24 18:37:37 +0700

    refac: moved all the test to api tests
    Reserving entry_test.rb for non webmock tests...

--- test/changelogs/entry_api_host_test.rb
+++ test/changelogs/entry_api_host_test.rb
@@ -2,8 +2,22 @@ require 'test_helper'
 
 module Changelogs
   class EntryApiHostTest < Minitest::Test
+    @@valid_args = {
+      event:       'C',
+      changeset:   '{}',
+      account_id:  123,
+      user_id:     234,
+      user_type:   'F',
+      logged_type: 'SomeModel',
+      logged_id:   345,
+      logged_at:   DateTime.parse('2001-02-03 04:05:06 UTC'),
+    }
+
     def setup
       WebMock.reset!
+
+      stub_request(:post, "https://changelogs.nimonikapp.com/entries").
+        to_return(status: 200, body: "", headers: {})
     end
 
     def teardown
@@ -27,6 +41,22 @@ module Changelogs
       entry = Entry.create()
 
       assert_requested :post, "https://changelogs.staging.nimonikapp.com/entries"
+    end
+
+    def test_that_it_creates_new_entry
+      entry = Entry.create(@@valid_args)
+
+      @@valid_args.each do |key, val|
+        assert_equal entry.public_send(key), val, "Wrong entry.#{key}"
+      end
+    end
+
+    def test_that_it_calls_api_to_create
+      entry = Entry.create(@@valid_args)
+
+      assert_requested :post, "https://changelogs.nimonikapp.com/entries" do |req|
+        req = '{"event":"C","changeset":"{}","account_id":123,"user_id":234,"user_type":"F","logged_type":"SomeModel","logged_id":345,"logged_at":"2001-02-03T04:05:06+00:00"}'
+      end
     end
 
     private
--- test/changelogs/entry_test.rb
+++ test/changelogs/entry_test.rb
@@ -2,41 +2,5 @@ require 'test_helper'
 
 module Changelogs
   class EntryTest < Minitest::Test
-    @@valid_args = {
-      event:       'C',
-      changeset:   '{}',
-      account_id:  123,
-      user_id:     234,
-      user_type:   'F',
-      logged_type: 'SomeModel',
-      logged_id:   345,
-      logged_at:   DateTime.parse('2001-02-03 04:05:06 UTC'),
-    }
-
-    def setup
-      WebMock.reset!
-      stub_request(:post, "https://changelogs.nimonikapp.com/entries").
-        to_return(status: 200, body: "", headers: {})
-    end
-
-    def teardown
-      Changelogs.subdomain = nil
-    end
-
-    def test_that_it_creates_new_entry
-      entry = Entry.create(@@valid_args)
-
-      @@valid_args.each do |key, val|
-        assert_equal entry.public_send(key), val, "Wrong entry.#{key}"
-      end
-    end
-
-    def test_that_it_calls_api_to_create
-      entry = Entry.create(@@valid_args)
-
-      assert_requested :post, "https://changelogs.nimonikapp.com/entries" do |req|
-        req = '{"event":"C","changeset":"{}","account_id":123,"user_id":234,"user_type":"F","logged_type":"SomeModel","logged_id":345,"logged_at":"2001-02-03T04:05:06+00:00"}'
-      end
-    end
   end
 end
